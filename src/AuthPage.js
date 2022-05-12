bd2ca06117a990ed63ed019eecbe0068680dfb1d
Cody-Wise <cody@sharptackmedia.com>
2022-05-12 10:56:08 -0700

    Set initial state on AuthPage

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import { useState } from 'react';
+import { signIn, signUp, getUser } from './services/fetch-utils';
 
-export default function AuthPage() {
+export default function AuthPage({ setUser }) {
+  const [{ email: signUpEmail, password: signUpPassword }, setSignUpFormDate] = useState({
+    email: '',
+    password: '',
+  });
+  const [{ email: signInEmail, password: signInPassword }, setSignInFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  async function handleSignUp() {}
+
+  async function handleSignIn() {}
   return <div>AuthPage</div>;
 }
